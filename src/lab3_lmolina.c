2aad9da4e59f2c13ab1237fe944eea70d3558be4
lmolinam <luis.molina@usach.cl>
2019-07-08 01:37:32 -0400

    Se agregan TDAs
    - métodos necesarios de creación y manipulación de estructuras
    - métodos para liberacion de memoria

--- src/lab3_lmolina.c
+++ src/lab3_lmolina.c
@@ -1,31 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 
 typedef struct{
     int index;
-    int* next;
-}ListaIndices;
+    ListaIndex* next;
+}ListaIndex;
 
 typedef struct{
-    int id;
+    int idGen;
 }Gen;
 
 typedef struct{
-    char[5];
-    Gen* gen; // si no inicializo esto es igual a NULL?
-    ListaIndices* padres; // si no inicializo esto es igual a NULL?
+    char idProceso[5];
+    int profundidad;
+    Gen* gen;
+    ListaIndex* padres;
 }ProcesoBio;
 
+ListaIndex* crearLista(int newIndex){
+    ListaIndex* new = (ListaIndex*)malloc(sizeof(ListaIndex));
+    new->index = newIndex;
+    new->next = NULL;
+    return new;
+}
 
-ProcesoBio* crearArregloProcesoBio(int size){
-    ProcesoBio* arreglo = (int*)malloc(sizeof(ProcesoBio) * size);
-    if (arreglo != NULL){
-        return arreglo;
-    }else{
-        printf("ERROR: No se encontro suficiente memoria para crear arreglo de procesos biológicos\n");
+
+ListaIndex* insertar(int newIndex, ListaIndex* lista){
+    ListaIndex* new = (ListaIndex*)malloc(sizeof(ListaIndex));
+    new->index = newIndex;
+    new->next = lista;
+    return new;
+}
+
+ListaIndex* siguiente(ListaIndex* lista, int posicion){
+
+    if(posicion < 0 || lista == NULL){
         return NULL;
     }
+
+    int contador = 1;
+    while (contador <= posicion){
+        contador++;
+        lista = lista->next;
+        if(lista == NULL){
+            return NULL;
+        }
+    }
+
+    return lista;
 }
 
 int* crearArregloGenes(int size){
@@ -39,7 +63,7 @@ int* crearArregloGenes(int size){
 }
 
 ProcesoBio* crearArregloProcesoBio(int size){
-    ProcesoBio* arreglo = (int*)malloc(sizeof(ProcesoBio) * size);
+    ProcesoBio* arreglo = (ProcesoBio*)malloc(sizeof(ProcesoBio) * size);
     if (arreglo != NULL){
         return arreglo;
     }else{
@@ -48,11 +72,83 @@ ProcesoBio* crearArregloProcesoBio(int size){
     }
 }
 
+void freeListaIndex(ListaIndex* lista){
+    int i;
+
+    while(lista != NULL){
+        ListaIndex* old = lista;
+        lista = lista->next;
+        free(old);
+    }
+}
+
+void freeContProcesoBio(ProcesoBio* procesoBio){
+    if (procesoBio != NULL){
+        free(procesoBio->gen);
+        freeListaIndex(procesoBio->padres);
+    }
+}
+
+void freeGrafo(ProcesoBio* procesoBio, int size){
+    for (int i = 0; i < size; i++){
+        freeContProcesoBio(&procesoBio[i]);
+    }
+    free(procesoBio);
+}
+
+
+void printGrafo(ProcesoBio* grafo, int size){
+
+    for (int i = 1; i < size; i++){
+        ProcesoBio vertice = grafo[i];
+        printf("%s ", vertice.idProceso);
+        printf("%s ", vertice.idProceso);
+    }
+    
+}
+
+// void printMatriz(int** matriz, int size){
+// 	int i,j;
+// 	for(i=0;i<size;i++){
+// 		for(j=0;j<size;j++){
+// 			printf("%d ",matriz[i][j]);
+// 		}
+// 		printf("\n");
+// 	}
+// }
+
+
+// int main(){
+//     FILE *archivo = fopen("grafo.in","r");
+
+//     if(archivo==NULL)
+//     {
+//         printf("ERROR: El archivo no se pudo abrir, escriba el nombre correctamente con su extension.");
+//         return 1;
+//     }
+//     int size;
+//     fscanf(archivo,"%d",&size);
+//     int **matrizAdyacencia=crearMatrizAdy(archivo,size);
+//     fclose(archivo);
+
+//     printMatriz(matrizAdyacencia,size);
+    
+    
+//     freeMatriz(matrizAdyacencia,size);
+
+//     return 0;
+// }
+
+
 ProcesoBio* cargarGenes(FILE *archivo){
 
 }
 
 ProcesoBio* cargarGrafo(FILE *archivo){
+
+}
+
+void imprimirGrafo(ProcesoBio* grafo){
 
 }
 
@@ -77,7 +173,7 @@ double calcularLeacockChorodow(ProcesoBio vertice1, ProcesoBio vertice2, int siz
 }
 
 //retorna indice de proceso si existe, sino retorna -1
-int existeProceso(char[5] idProceso, ProcesoBio* grafo){
+int existeProceso(char idProceso[5], ProcesoBio* grafo){
 
 }
 
@@ -86,7 +182,7 @@ bool existeGen(Gen gen, ProcesoBio* grafo){
 }
 
 double calcularSimilitudGenes(Gen gen1, Gen gen2, ProcesoBio* grafo){
-    if !(existeGen(gen1, grafo) && existeGen(gen2, grafo)){
+    if (!(existeGen(gen1, grafo) && existeGen(gen2, grafo))){
         return -1;
     }
 }
