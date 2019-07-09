4dd57a86228e14c20806fcfb644aa1b4078e026d
lmolinam <luis.molina@usach.cl>
2019-07-09 15:57:43 -0400

    Funcionalidad cargar genes
    - Se cambia manejo de genes desde arreglo dinámico a listas enlazadas.
    - Se agrega funcionalidad para cargar procesos de grafo con genes desde archivo.
    - Se agrega funcionalidad para obtener los procesos biológicos a los que un gen pertenece.
    - Se avanzan funcionalidades para calcular similitud entre genes por Wu-Palmer y Leacock-Chorodow
    - Se agrega funcionalidad para mostrar grafo y todo su contenido por pantalla.
    - Se avanza cuerpo principal e interfaz del programa.

--- src/lab3_lmolina.c
+++ src/lab3_lmolina.c
@@ -4,23 +4,25 @@
 #include <string.h>
 
 
-typedef struct{
+typedef struct ListaIndex{
     int index;
-    ListaIndex* next;
+    struct ListaIndex* next;
 }ListaIndex;
 
-typedef struct{
+typedef struct Gen{
     int idGen;
+    struct Gen* next;
 }Gen;
 
 typedef struct{
     char idProceso[5];
     int profundidad;
-    Gen* gen;
+    Gen* genes;
     ListaIndex* padres;
 }ProcesoBio;
 
 typedef struct{
+    int cantidadVertices;
     int profundidad;
     ProcesoBio* vertices;
 }Grafo;
@@ -36,14 +38,14 @@ ListaIndex* crearListaIndex(int newIndex){
 }
 
 
-ListaIndex* insertar(int newIndex, ListaIndex* lista){
+ListaIndex* insertarListaIndex(int newIndex, ListaIndex* lista){
     ListaIndex* new = (ListaIndex*)malloc(sizeof(ListaIndex));
     new->index = newIndex;
     new->next = lista;
     return new;
 }
 
-ListaIndex* siguiente(ListaIndex* lista, int posicion){
+ListaIndex* siguienteListaIndex(ListaIndex* lista, int posicion){
 
     if(posicion < 0 || lista == NULL){
         return NULL;
@@ -61,15 +63,18 @@ ListaIndex* siguiente(ListaIndex* lista, int posicion){
     return lista;
 }
 
-int crearGen(int idGen){
-    Gen* gen = (Gen*)malloc(sizeof(Gen));
-    if (gen != NULL){
-        gen->idGen = idGen;
-        return gen;
-    }else{
-        printf("ERROR: No se encontro suficiente memoria para crear Gen\n");
-        return NULL;
-    }
+Gen* crearListaGen(int idGen){
+    Gen* new = (Gen*)malloc(sizeof(Gen));
+    new->idGen = idGen;
+    new->next = NULL;
+    return new;
+}
+
+Gen* insertarListaGen(int idGen, Gen* lista){
+    Gen* new = (Gen*)malloc(sizeof(Gen));
+    new->idGen = idGen;
+    new->next = lista;
+    return new;
 }
 
 Gen* crearArregloGenes(int size){
@@ -83,18 +88,9 @@ Gen* crearArregloGenes(int size){
 }
 
 ProcesoBio crearProcesoBio(char idProceso[5], int profundidad){
-    ProcesoBio* proceso = (ProcesoBio*)malloc(sizeof(ProcesoBio));
-    if (proceso != NULL){
-        strcpy(proceso->idProceso, idProceso);
-        proceso->profundidad = profundidad;
-        proceso->gen = NULL;
-        proceso->padres = NULL;
-
-        return proceso;   
-    }else{
-        printf("ERROR: No se encontro suficiente memoria para crear Proceso biológico\n");
-        return NULL;
-    }
+    ProcesoBio newProceso = {"", profundidad, NULL, NULL};
+    strcpy(newProceso.idProceso, idProceso); 
+    return newProceso;
 }
 
 ProcesoBio* crearArregloProcesoBio(int size){
@@ -112,6 +108,7 @@ Grafo* crearGrafo(int cantidadVertices){
     if (grafo != NULL){
         ProcesoBio* arregloProcesos = crearArregloProcesoBio(cantidadVertices);
         if(arregloProcesos != NULL){
+            grafo->cantidadVertices = cantidadVertices;
             grafo->vertices = arregloProcesos;
             grafo->profundidad = -1;
         }else{
@@ -132,15 +129,24 @@ int obtenerIndiceProcesoBio(char identificador[5], ProcesoBio* grafo, int size){
         }
     }
 
+    printf("Proceso %s no encontrado", identificador);
     return -1;
 }
 
 
 //***OPERACIONES DE LIMPIEZA DE MEMORIA***
 
+void freeGenes(Gen* genes){
+    int i;
+    while(genes != NULL){
+        Gen* old = genes;
+        genes = genes->next;
+        free(old);
+    }
+}
+
 void freeListaIndex(ListaIndex* lista){
     int i;
-
     while(lista != NULL){
         ListaIndex* old = lista;
         lista = lista->next;
@@ -150,94 +156,108 @@ void freeListaIndex(ListaIndex* lista){
 
 void freeContProcesoBio(ProcesoBio* procesoBio){
     if (procesoBio != NULL){
-        free(procesoBio->gen);
+        freeGenes(procesoBio->genes);
         freeListaIndex(procesoBio->padres);
     }
 }
 
-void freeGrafo(Grafo* grafo, int size){
-    for (int i = 0; i < size; i++){
+void freeGrafo(Grafo* grafo){
+    for (int i = 0; i < grafo->cantidadVertices; i++){
         freeContProcesoBio(&grafo->vertices[i]);
     }
-    free(&grafo->vertices);
+    free(grafo->vertices);
     free(grafo);
 }
 
 
-void printGrafo(ProcesoBio* grafo, int size, int profundidad){
-
-    for (int i = 0; i < size; i++){
-        ProcesoBio vertice = grafo[i];
-        if (vertice.padres == NULL){
-            printf("%s\n", vertice.idProceso);
-            continue;
-        }
-
-        printf("%s ", vertice.idProceso);
-        printf("%s ", vertice.idProceso);
+void printGenes(Gen* genes){
+    printf("| genes: ");
+    while(genes != NULL){
+        printf("%d ", genes->idGen);
+        genes = genes->next;
     }
-    
 }
 
+void printPadres(ListaIndex* padres, ProcesoBio* vertices, int cantidadVertices){
+    while(padres != NULL){
+        for (int i = 0; i < cantidadVertices; i++){
+            if(padres->index == i){          
+                printf("%s ", vertices[i].idProceso);
+            }
+        }
+        padres = padres->next;
+    }
+}
 
-// int main(){
-//     FILE *archivo = fopen("grafo.in","r");
-
-//     if(archivo==NULL)
-//     {
-//         printf("ERROR: El archivo no se pudo abrir, escriba el nombre correctamente con su extension.");
-//         return 1;
-//     }
-//     int size;
-//     fscanf(archivo,"%d",&size);
-//     int **matrizAdyacencia=crearMatrizAdy(archivo,size);
-//     fclose(archivo);
-
-//     printMatriz(matrizAdyacencia,size);
-    
-    
-//     freeMatriz(matrizAdyacencia,size);
-
-//     return 0;
-// }
-
+void printGrafo(Grafo* grafo){
+    printf("profundidad grafo: %d \n", grafo->profundidad);
+    printf("total vertices: %d \n", grafo->cantidadVertices);
+    for (int i = 0; i < grafo->cantidadVertices; i++){
+        if (grafo->vertices[i].padres == NULL){
+            printf("raiz: %s ", grafo->vertices[i].idProceso);
+            printf("| profundidad: %d ", grafo->vertices[i].profundidad);
+            if(grafo->vertices[i].genes != NULL){
+                printGenes(grafo->vertices[i].genes);
+            }
+            printf("%c", '\n');
+        }else{
+            printf("%s ", grafo->vertices[i].idProceso);
+            printf("| profundidad: %d ", grafo->vertices[i].profundidad);
+            if(grafo->vertices[i].genes != NULL){
+                printGenes(grafo->vertices[i].genes);
+            }
+            printf("| padres: ");
+            printPadres(grafo->vertices[i].padres, grafo->vertices, grafo->cantidadVertices);
+            printf("%c", '\n');
+        }
+    }
+}
 
 Grafo* cargarGrafo(FILE *archivo){
     int size;
     char idProceso[5];
     char chr;
     fscanf(archivo,"%d",&size);
-    fscanf(archivo,"%s", &idProceso);
     Grafo* grafo = crearGrafo(size);
-    strcpy(grafo->vertices[0].idProceso, idProceso);
-    grafo->vertices[0].profundidad = 0;
-    grafo->vertices[0].padres = NULL;
-    grafo->vertices[0].gen = NULL;
     int palabraCount = 0;
 
-    for (int i = 1; i < size; i++){
-        while(fscanf(archivo, "%s%c", &idProceso, &chr) != EOF){
+    for (int i = 0; i < size; i++){
+        while(fscanf(archivo, "%s%c", idProceso, &chr) != EOF){
             if(palabraCount == 0){
                 //Es un nuevo vértice
-                ProcesoBio newProceso = {idProceso, -1, NULL, NULL};
+                ProcesoBio newProceso = crearProcesoBio(idProceso, -1);
                 grafo->vertices[i] = newProceso;
                 palabraCount++;
             }else if(palabraCount == 1){
                 //Primer padre del vértice
                 int indicePadre = obtenerIndiceProcesoBio(idProceso, grafo->vertices, i+1);
-                grafo->vertices[i].padres = crearListaIndex(indicePadre);
-                grafo->vertices[i].profundidad = grafo->vertices[indicePadre].profundidad + 1;
+                if (indicePadre != -1){
+                    grafo->vertices[i].padres = crearListaIndex(indicePadre);
+                    grafo->vertices[i].profundidad = grafo->vertices[indicePadre].profundidad + 1;
+                }else{
+                    printf("No se encontro proceso %s para ser padre de proceso %s", idProceso, grafo->vertices[i].idProceso);
+                }
+
+                palabraCount++;
+                if(grafo->profundidad < grafo->vertices[i].profundidad){
+                    grafo->profundidad = grafo->vertices[i].profundidad;
+                }
             }else{
                 //Más padres
                 int indicePadre = obtenerIndiceProcesoBio(idProceso, grafo->vertices, i+1);
-                grafo->vertices[i].padres = insertar(indicePadre, grafo->vertices[i].padres);
-                int profundidadTemp = grafo->vertices[indicePadre].profundidad + 1;
-                if(profundidadTemp > grafo->vertices[i].profundidad){
-                    grafo->vertices[i].profundidad = profundidadTemp;
+                if (indicePadre != -1){
+                    grafo->vertices[i].padres = insertarListaIndex(indicePadre, grafo->vertices[i].padres);
+                }else{
+                    printf("No se encontro proceso %s para ser padre de proceso %s", idProceso, grafo->vertices[i].idProceso);
                 }
             }
 
-            if(chr = '\n'){
+            if(chr == '\n'){
+                if(palabraCount == 1){
+                    //vertice es raiz
+                    grafo->vertices[i].profundidad = 0;
+                    grafo->profundidad = 0;
+                }
                 palabraCount = 0;
                 break;
             }
@@ -245,77 +265,175 @@ Grafo* cargarGrafo(FILE *archivo){
     }
 
     return grafo;
-    
 }
 
-ProcesoBio* cargarGenes(FILE *archivo){
+// Función para cargar genes en grafo
+Grafo* cargarGenes(FILE *archivo, Grafo* grafo){
+    int size;
+    char id[5];
+    char chr;
+    int palabraCount = 0;;
+    fscanf(archivo,"%d",&size);
+    // printf("cantidad genes: %d", size);
+    // Gen* genes = crearArregloGenes(size);
 
-}
+    for (int i = 0; i < size; i++){
+        while(fscanf(archivo, "%s%c", id, &chr) != EOF){
+            if(palabraCount == 0){
+                //Es un nuevo gen
+                palabraCount++;
+                continue;
+            }else if(palabraCount >= 1){
+                //Proceso al que pertenece el gen
+                int indiceProceso = obtenerIndiceProcesoBio(id, grafo->vertices, grafo->cantidadVertices);
+                if(indiceProceso != -1){
+                    if(grafo->vertices[indiceProceso].genes == NULL){
+                        grafo->vertices[indiceProceso].genes = crearListaGen(i+1);
+                    }else{
+                        grafo->vertices[indiceProceso].genes = insertarListaGen(i+1, grafo->vertices[indiceProceso].genes);    
+                    }
+                }else{
+                    printf("No se encontro proceso %s para relacionar con gen G%d", id, i+1);
+                }
+                palabraCount++;
+            }
 
-void imprimirGrafo(ProcesoBio* grafo){
-
-}
-
-ProcesoBio encontrarAncenstroComun(ProcesoBio vertice1, ProcesoBio vertice2, int size){
-
-}
-
-int determinarLongitudCamino(ProcesoBio hijo, ProcesoBio ancestro, int size){
-
-}
-
-int determinarProfundidadGrafo(ProcesoBio* grafo){
-
-}
-
-double calcularWuPalmer(ProcesoBio vertice1, ProcesoBio vertice2, int size){
-
-}
-
-double calcularLeacockChorodow(ProcesoBio vertice1, ProcesoBio vertice2, int size){
-
-}
-
-//retorna indice de proceso si existe, sino retorna -1
-int existeProceso(char idProceso[5], ProcesoBio* grafo){
-
-}
-
-bool existeGen(Gen gen, ProcesoBio* grafo){
-
-}
-
-double calcularSimilitudGenes(Gen gen1, Gen gen2, ProcesoBio* grafo){
-    if (!(existeGen(gen1, grafo) && existeGen(gen2, grafo))){
-        return -1;
+            if(chr == '\n'){
+                palabraCount = 0;
+                break;
+            }
+        }
     }
+
+    return grafo;
+
+}
+
+ListaIndex* obtenerProcesosGen(Grafo grafo, int idGen){
+    ListaIndex* listaIndices = NULL;
+    int encontrados = 0;
+
+    for (int i = 0; i < grafo.cantidadVertices; i++){
+        while(grafo.vertices[i].genes != NULL){
+            if(grafo.vertices[i].genes->idGen == idGen){
+                if(encontrados == 0){
+                    listaIndices = crearListaIndex(i);
+                }else{
+                    listaIndices = insertarListaIndex(i, listaIndices);
+                }
+            }
+
+            grafo.vertices[i].genes = grafo.vertices[i].genes->next;
+        }
+    }
+
+    return listaIndices;
 }
 
 
-int similitudGenes(){
-    FILE *archivoProcesos = fopen("input/procesos.in","r");
+ProcesoBio encontrarAncenstroComun(ProcesoBio vertice1, ProcesoBio vertice2, Grafo grafo){
     
-    if(archivoProcesos==NULL)
-    {
-        printf("ERROR: El archivo procesos.in no se pudo abrir, escriba el nombre correctamente con su extension.");
+}
+
+double calcularWuPalmer(ProcesoBio vertice1, ProcesoBio vertice2, Grafo grafo){
+
+}
+
+double calcularLeacockChorodow(ProcesoBio vertice1, ProcesoBio vertice2, Grafo grafo){
+
+}
+
+double calcularSimilitudLeacockChorodow(int idGen1, int idGen2, Grafo grafo){
+    double resultado = -1;
+    ListaIndex* procesosGen1 = obtenerProcesosGen(grafo, idGen1);
+    ListaIndex* procesosGen2 = obtenerProcesosGen(grafo, idGen2);
+
+    if (!(procesosGen1 != NULL && procesosGen2 != NULL)){
+        return resultado;
+    }
+
+    while(procesosGen1 != NULL){
+        ListaIndex* procesosGen2Cpy = procesosGen2;
+        while (procesosGen2Cpy != NULL){
+            resultado += calcularWuPalmer(grafo.vertices[procesosGen1->index], grafo.vertices[procesosGen2->index], grafo);
+        }
+        
+    }
+    
+    return resultado;
+}
+
+double calcularSimilitudWuPalmer(int idGen1, int idGen2, Grafo grafo){
+    double resultado = -1;
+    ListaIndex* procesosGen1 = obtenerProcesosGen(grafo, idGen1);
+    ListaIndex* procesosGen2 = obtenerProcesosGen(grafo, idGen2);
+
+    if (!(procesosGen1 != NULL && procesosGen2 != NULL)){
+        return resultado;
+    }
+
+    while(procesosGen1 != NULL){
+        ListaIndex* procesosGen2Cpy = procesosGen2;
+        while (procesosGen2Cpy != NULL){
+            resultado += calcularLeacockChorodow(grafo.vertices[procesosGen1->index], grafo.vertices[procesosGen2->index], grafo);
+        }
+        
+    }
+    
+    return resultado;
+}
+
+FILE* cargarArchivo(char* path){
+    FILE* archivo = fopen(path,"r");
+
+    if(archivo == NULL){
+        printf("ERROR: El archivo %s no se pudo abrir, escriba el nombre correctamente con su extension.", path);
+        return NULL;
+    }
+
+    return archivo;
+}
+
+int calcularSimilitudGenes(){
+    FILE* archivoProcesos = cargarArchivo("input/procesos.in");
+    FILE* archivoGenes = cargarArchivo("input/genes.in");
+    
+    if(archivoProcesos == NULL || archivoGenes == NULL){
         return 1;
     }
 
-    ProcesoBio* grafo = cargarGrafo(archivoProcesos);
-
-    int size;
-    fscanf(archivoProcesos,"%d",&size);
-    int **matrizAdyacencia=crearMatrizAdy(archivoProcesos,size);
+    Grafo* grafo = cargarGrafo(archivoProcesos);
     fclose(archivoProcesos);
+    grafo = cargarGenes(archivoGenes, grafo);
+    fclose(archivoGenes);
+    printGrafo(grafo);
 
-    printMatriz(matrizAdyacencia,size);
-    
-    
-    freeMatriz(matrizAdyacencia,size);
+    int gen1;
+    int gen2;
+    double similitudWP;
+    double similitudLC;
+    char* confirmacion; 
+    do{
+        printf("Ingrese primer gen: ");
+        scanf("%d", &gen1);
+        printf("\nIngrese segundo gen: ");
+        scanf("%d", &gen2);
+        
+        similitudWP = calcularSimilitudWuPalmer(gen1, gen2, *grafo);
+        similitudLC = calcularSimilitudLeacockChorodow(gen1, gen2, *grafo);
+        
+        printf("\nSimilitud de genes Wu-Palmer: %lf", similitudWP);
+        printf("\nSimilitud de genes Leacok-Chorodow: %lf", similitudLC);
+        printf("\n¿Desea ingresar otro par de genes?: ");
+        scanf("%s", confirmacion);
+
+    } while (strcmp(confirmacion, "No") != 0);
+
+    freeGrafo(grafo);
 
     return 0;
 }
 
 int main(){
-    return similitudGenes();
+    return calcularSimilitudGenes();
 }
